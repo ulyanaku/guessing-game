247719c0b5a1bec4a071568cd3cabf7a849cddd0
Ulyana <31891478+ulyanaku@users.noreply.github.com>
2017-09-28 21:42:14 +0300

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -16,11 +16,11 @@ class GuessingGame {
     }
 
     lower() {
-        this.max=this.guessNum;
+        this.max=this.guessNum+1;
     }
 
     greater() {
-        this.min=this.guessNum;
+        this.min=this.guessNum-1;
     }
 }
 
