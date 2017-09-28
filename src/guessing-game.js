c9d6e1e5c3c7f8f0c2d82e2fc6a92cee0e55401a
Ulyana <31891478+ulyanaku@users.noreply.github.com>
2017-09-28 21:32:30 +0300

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -11,7 +11,7 @@ class GuessingGame {
     }
 
     guess() {
-        this.guessNum=Math.floor((this.min+this.max)/2);
+        this.guessNum=Math.floor(this.min-(this.min+this.max)/2);
         return this.guessNum;
     }
 
